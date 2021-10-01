e7e54814e179b7118b67baa12cad2b69a772d6bd
julivheuvel <juli.heuvel@gmail.com>
2021-10-01 10:57:20 -0400

    commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,6 +6,8 @@ const About = () => {
     return (
         <div className="about">
             <h1>Explore Co About Page</h1>
+
+            <div>display background image here</div>
         </div>
     )
 }
