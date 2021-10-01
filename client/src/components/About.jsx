76c9df478e9dd192a69ccf887f950e7e9c9f0e0c
julivheuvel <juli.heuvel@gmail.com>
2021-10-01 16:10:48 -0400

    commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,10 +7,22 @@ const About = () => {
 
     return (
         <div className="about">
+            
             <NavBar />
-            <h1>Explore Co About Page</h1>
-
-            <div>display background image here</div>
+            <div className="about-content">
+                <h1>Our Story</h1>
+                <div>Established 2021</div>
+                <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quasi alias explicabo ea voluptate officia, rem nihil minima magnam, soluta asperiores quam mollitia eaque harum illo amet molestiae fuga dicta. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et esse rem, consequuntur dolore vitae totam inventore excepturi cum? Eaque fugiat esse facilis officia suscipit quo dignissimos quibusdam quae? Tempore, officia?</div>
+                <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis, corporis aspernatur veniam sequi earum consequatur magnam magni! Magnam nisi, facilis quae veritatis consequatur quasi eos esse doloribus illum, laboriosam nam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quis unde alias sapiente rerum dolore, esse in quos fuga voluptatem. Sapiente facilis ipsa vitae! Repudiandae hic similique doloribus laboriosam molestias. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis inventore explicabo ipsum doloremque nobis. Porro excepturi illum inventore aliquid placeat possimus voluptas qui non? Ullam quos consequatur eligendi harum odit.</div>
+            
+                <h2>Content Creators</h2>
+                <h3>Aaron Vanden Heuvel</h3>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quae ipsa optio dolorem, animi ipsam distinctio esse repellat hic perspiciatis commodi voluptatem, maiores eveniet. Autem ipsam tempora aut fuga voluptatibus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi nam eos corporis incidunt sequi corrupti eum rerum nemo? Consequatur tempore ea odit! Aspernatur nesciunt est repudiandae labore repellendus consequatur. A. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis accusantium, hic voluptatibus veniam voluptate nostrum non esse facilis similique ipsam error, alias tempore doloribus! Consequuntur nemo doloremque odio voluptatem praesentium?</div>
+                <h3>Juli Vanden Heuvel</h3>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corporis sapiente velit enim odit eos, dolor numquam maxime officiis laboriosam. Possimus tempora eveniet sapiente, quis voluptatibus minus dolores excepturi quibusdam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. In odit id eum molestias quisquam impedit debitis ab similique, nam qui repellat et illo fugiat asperiores est, quasi provident nostrum ea? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis culpa dicta sunt nobis autem perspiciatis ex, temporibus eveniet. Alias at iste ex quos aliquid nisi, in magni laboriosam unde ipsa.</div>
+                <h3>Kyle Rutkowski</h3>
+                    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non aliquid veniam deserunt. Similique laboriosam doloribus perspiciatis vitae qui unde culpa quibusdam excepturi corrupti fugit? Quaerat blanditiis quae placeat corrupti sequi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis fugiat est voluptates unde perferendis. Aut placeat, debitis hic reprehenderit nihil eius nemo error cum quisquam asperiores atque itaque quia aliquid! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci unde nesciunt veniam et sit explicabo iste quidem similique! Nulla praesentium libero cumque dolore, quae eveniet inventore at in quia? Sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, provident unde quam consectetur repellendus eius, sed iste est quisquam architecto minus, vel at omnis illum natus cupiditate quae iure dolores.</div>
+            </div>
         </div>
     )
 }
