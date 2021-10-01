2f4441ae3159382217243b63f00770144f00f379
julivheuvel <juli.heuvel@gmail.com>
2021-10-01 10:54:21 -0400

    commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -8,12 +8,7 @@ const Login = () => {
     return (
         <div className="login">
             <h1>Explore Co Login Page</h1>
-<<<<<<< HEAD
-
-            <Link to="/register">Register</Link>
-=======
-            <h2>Just testing to see if this works</h2>
->>>>>>> 66fa02ae0e606958ab53a6201a1cac863cf5c6f0
+            <Link to="/register">register</Link>
         </div>
     )
 }
