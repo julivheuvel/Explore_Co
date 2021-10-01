66fa02ae0e606958ab53a6201a1cac863cf5c6f0
Keeylay <kyrutkowski@gmail.com>
2021-10-01 09:26:30 -0500

    Added h2 for git testing

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,6 +6,7 @@ const Login = () => {
     return (
         <div className="login">
             <h1>Explore Co Login Page</h1>
+            <h2>Just testing to see if this works</h2>
         </div>
     )
 }
