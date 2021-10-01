2ed87c3b34e127023f5d27777bf906872b194bf7
julivheuvel <juli.heuvel@gmail.com>
2021-10-01 14:00:37 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
 import React from 'react';
 import { Router } from '@reach/router';
 
+// CSS
+import './css/About.css';
+import './css/Contact.css';
+import './css/Index.css';
+import './css/Login.css';
+import './css/NavBar.css';
+import './css/Questionaire.css';
+import './css/Register.css';
+import './css/Services.css';
+
 // components
-import Index from './components/Index';
 import About from './components/About';
-import Services from './components/Services';
 import Contact from './components/Contact';
+import Index from './components/Index';
 import Login from './components/Login';
-import Register from './components/Register';
 import Questionaire from './components/Questionaire';
+import Register from './components/Register';
+import Services from './components/Services';
 
 
 function App() {
@@ -18,13 +27,13 @@ function App() {
       
       
       <Router>
-        <Index path="/" />
         <About path="/about-us" />
-        <Services path="/services" />
         <Contact path="/contact-us" />
+        <Index path="/" />
         <Login path="/login" />
-        <Register path="/register" />
         <Questionaire path="/questionaire" />
+        <Register path="/register" />
+        <Services path="/services" />
 
       </Router>
     </div>
