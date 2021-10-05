8259460172d10818e450c8fcacc6c791befd863a
Keeylay <kyrutkowski@gmail.com>
2021-10-04 19:10:27 -0500

    NavBar

--- client/src/web-layout-components/NavBar.jsx
+++ client/src/web-layout-components/NavBar.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "@reach/router";
+import axios from "axios";
 
 const NavBar = () => {
 
+    const [loggedinuser, setLoggedInUser] = useState({})
+
+    useEffect( () => {
+        axios.get("http://localhost8000:/api/users/getloggedinuser", {withCredentials:true})
+            .then(res => {
+                console.log("logged in user info", res)
+                setLoggedInUser(res.data)
+            })
+            .catch(err => {
+                console.log("error with navigation")
+            })
+    })
 
     return (
         <div className="navbar">
@@ -14,7 +27,13 @@ const NavBar = () => {
                 <Link to="/about-us" className="nav-link">ABOUT</Link>
                 <Link to="/services" className="nav-link">SERVICES</Link>
                 <Link to="/contact-us" className="nav-link">CONTACT</Link>
-                <Link to="/login" className="nav-link">LOGIN</Link>
+                {!loggedinuser?
+                <>
+                <p className="nav-link">{loggedinuser.parent_first_name}</p>
+                <Link to="/dashboard" className="nav-link">Logout</Link>
+                </>:<Link to="/login" className="nav-link">LOGIN</Link>
+                }
+
             </div>
         </div>
     )
